500eaf14b5bedbf16f7cced3bbdb545576b9ecbb
jherson <jherson.o.s@hotmail.com>
2026-01-04 01:59:46 -0500

    version 3.1.2

--- src/server.js
+++ src/server.js
@@ -1,9 +1,21 @@
 import "dotenv/config";
 import buildApp from "./app.js";
+import path from "path";
+import { fileURLToPath } from "url";
+import fastifyStatic from "@fastify/static";
 
 const app = await buildApp();
 
 const PORT = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// ⬇️ AGREGA ESTO
+server.register(fastifyStatic, {
+  root: path.join(__dirname, "public"),
+  prefix: "/", // ← MUY IMPORTANTE
+});
 
 app.listen({ port: PORT, host: "0.0.0.0" }, () => {
   console.log(`🚀 CWA backend running on port ${PORT}`);
